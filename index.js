98961304fe97127a00a75dfa50af911b4cc406ce
Matthew Hunger <matt.hunger@gmail.com>
2017-08-31 12:04:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function divide (a,b) {
   return a/b
 }
 
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function 
